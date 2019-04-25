e47afb8f608995b04720d28cf8ac215149ec7f52
DESKTOP-AV9NNBP\MijalH <kyrcooler@gmail.com>
2019-04-25 23:53:28 +0200

    Initial commit

---
+++ DNA_sequencing/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DNA_sequencing
+{
+    class Program
+    { 
+        public const string FileName = @"CurrentDNA.txt";
+
+        static void Main(string[] args)
+        {
+            var dnaSequence = string.Empty;
+            Console.WriteLine("Generate new DNA sequence (g)\nLoad in DNA seqence (l)");
+            var option =  Console.ReadLine();
+
+            if (option == "g")
+            {
+                Console.WriteLine("How long should the DNA sequence be: ");
+
+                var correctNumberInput = Int32.TryParse(Console.ReadLine(), out var dnaLength);
+                if (!correctNumberInput)
+                {
+                    dnaLength = 10;
+                }
+
+                dnaSequence = StringHelper.GetRandomString(dnaLength);
+                System.IO.File.WriteAllText(FileName, dnaSequence);
+            }
+            else
+            {
+                try
+                {
+                    dnaSequence = System.IO.File.ReadAllText(FileName);
+                }
+                catch
+                {
+                    Console.WriteLine("No DNA file");
+                }
+            }
+            
+            //try to recreate this DNA
+            //for now let's compare to sample string
+
+            var sampleDna = "AAAAAAAAGGGGTACAGCCC";
+
+            StringHelper.CompareString(dnaSequence, sampleDna);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DNA_sequencing/StringHelper.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace DNA_sequencing
+{
+    public class StringHelper
+    {
+        const string pool = "ACGT";
+
+        private static int seed;
+
+        private static ThreadLocal<Random> threadLocal = new ThreadLocal<Random>(() => new Random(Interlocked.Increment(ref seed)));
+        
+        static StringHelper()
+        {
+            seed = Environment.TickCount;
+        }
+        
+        public static Random Instance => threadLocal.Value;
+
+        //Methods to use:
+        public static string GetRandomString(int length)
+        {
+            var chars = Enumerable.Range(0, length)
+                .Select(x => pool[Instance.Next(0, pool.Length)]);
+
+            return new string(chars.ToArray());
+        }
+
+        public static void CompareString(string baseString, string referenceString)
+        {
+            InitializeMatrix(baseString.Length, referenceString.Length, out var scoringMatrix);
+            FillScoresInMatrix(scoringMatrix, baseString, referenceString);
+            var overallScore = CalculateScore(scoringMatrix, baseString, referenceString);
+            Console.WriteLine("Score: " + overallScore);
+        }
+
+        private static void InitializeMatrix(int baseLength, int referenceLength, out int[,]scoringMatrix)
+        {
+            scoringMatrix = new int[++baseLength, ++referenceLength];
+
+            //Initialization Step - filled with 0 for the first row and the first column of matrix
+            for (int i = 0; i < baseLength; i++)
+            {
+                scoringMatrix[i, 0] = 0;
+            }
+
+            for (int j = 0; j < referenceLength; j++)
+            {
+                scoringMatrix[0, j] = 0;
+            }
+        }
+
+        private static void FillScoresInMatrix(int[,]scoringMatrix, string baseString, string referenceString)
+        {
+            for (int i = 1; i < scoringMatrix.GetLength(0); i++) //each element in base string
+            {
+                for (int j = 1; j < scoringMatrix.GetLength(1); j++) //each element in reference string
+                {
+                    var scoreDiag = referenceString.Substring(j - 1, 1) == baseString.Substring(i - 1, 1)
+                        ? scoringMatrix[i - 1, j - 1] + 2
+                        : scoringMatrix[i - 1, j - 1] + -1;
+
+                    var scoreLeft = scoringMatrix[i, j - 1] - 2;
+                    var scoreUp = scoringMatrix[i - 1, j] - 2;
+
+                    var maxScore = Math.Max(Math.Max(scoreDiag, scoreLeft), scoreUp);
+
+                    scoringMatrix[i, j] = maxScore;
+                }
+            } 
+        }
+
+        private static int CalculateScore(int[,]scoringMatrix, string baseString, string referenceString)
+        {
+            var baseElements = baseString.ToCharArray();
+            var referenceElements = referenceString.ToCharArray();
+
+            var alignmentA = string.Empty;
+            var alignmentB = string.Empty;
+            var m = baseString.Length;
+            var n = referenceString.Length;
+            var overallScore = 0;
+
+            while (m > 0 && n > 0)
+            {
+                int scoreDiag;
+
+                //is +2 for a match, -1 for a mismatch and -2 for a gap
+                if (baseElements[m - 1] == referenceElements[n - 1])
+                    scoreDiag = 2;
+                else
+                    scoreDiag = -1;
+                
+                overallScore += scoringMatrix[m, n];
+
+                if (m > 0 && n > 0 && scoringMatrix[m, n] == scoringMatrix[m - 1, n - 1] + scoreDiag)
+                {
+                    alignmentA = referenceElements[n - 1] + alignmentA;
+                    alignmentB = baseElements[m - 1] + alignmentB;
+                    m = m - 1;
+                    n = n - 1;
+                }
+                else if (n > 0 && scoringMatrix[m, n] == scoringMatrix[m, n - 1] - 2)
+                {
+                    alignmentA = referenceElements[n - 1] + alignmentA;
+                    alignmentB = "-" + alignmentB;
+                    n = n - 1;
+                }
+                else //if (m > 0 && scoringMatrix[m, n] == scoringMatrix[m - 1, n] + -2)
+                {
+                    alignmentA = "-" + alignmentA;
+                    alignmentB = baseElements[m - 1] + alignmentB;
+                    m = m - 1;
+                }
+            }
+            
+            Console.WriteLine("------");
+            Console.WriteLine(alignmentA);
+            Console.WriteLine(alignmentB);
+
+            return overallScore;
+        }
+    }
+}
