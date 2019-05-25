4eabfc4d1322dded97a040ccba668ae5da72a7e0
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2019-05-25 22:42:27 +0200

    Add validation for input sequence

--- DNA_sequencing/Program.cs
+++ DNA_sequencing/Program.cs
@@ -5,6 +5,8 @@ namespace DNA_sequencing
 {
     class Program
     { 
+        static readonly char[] ValidNucleotides =  new char[] {'A', 'C', 'G', 'T'};
+
         static void Main(string[] args)
         {
             int k = int.Parse(args[1]);
@@ -14,7 +16,13 @@ namespace DNA_sequencing
         }
 
         private static string ReadSequenceInFile(string filename) {
-            return "ACGT";
+            ValidateFile(filename);
+
+            string sequence = ReadContentsOfFile(filename);
+
+            ValidateSequence(sequence);
+
+            return sequence;
         }
 
         private static string SequenceDNA(string baseSequence, int k) {
@@ -27,6 +35,35 @@ namespace DNA_sequencing
             Console.WriteLine($"Original sequence:\t{originalSequence}");
             Console.WriteLine($"Generated sequence:\t{generatedSequence}");
             Console.WriteLine($"Similarity: {similarity * 100}%");
+        }
+
+        private static void ValidateFile(string filename) {
+            if (!File.Exists(filename)) {
+                Console.WriteLine("File doesn't exist or you don't have permission to read it.");
+                Environment.Exit(1);
+            }
+        }
+
+        private static string ReadContentsOfFile(string filename) {
+            return File.ReadAllText(filename).Trim();
+        }
+
+        private static void ValidateSequence(string sequence) {
+            if (sequence.Length == 0) {
+                Console.WriteLine("The file is empty.");
+                Environment.Exit(2);
+            }
+
+            char[] sequenceCharacters = sequence.ToCharArray();
+            foreach (var character in sequenceCharacters)
+            {
+                bool isValidCharacter = Array.IndexOf(ValidNucleotides, character) != -1;
+
+                if (!isValidCharacter) { 
+                    Console.WriteLine($"Sequence contains invalid character: {character}.");
+                    Environment.Exit(2);
+                }
+            }
         }
     }
 }
